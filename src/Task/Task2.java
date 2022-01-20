84f14c65b74bb1d75b0a1c888ee28a8a6193ea7f
Mona Lisa <truman5555>
2022-01-21 00:06:51 +0300

    commit 1

---
+++ src/Task/Task2.java
@@ -0,0 +1,35 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+ //        System.out.println("Enter number1:"+ " ");
+ //       int number1 = scanner.nextInt();
+  //      System.out.println("Enter number2:"+ " ");
+  //      int number2 = scanner.nextInt();
+
+ //       if (number1%number2==0)
+ //       {
+ //          System.out.println("multipl");
+ //       }
+ //       else {
+  //          System.out.println("not multipl");
+ //       }
+
+
+        System.out.println("Enter number1:"+ " ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter number2:"+ " ");
+        int num2 = scanner.nextInt();
+
+        String multipl=num1%num2==0? "Multipl":"Not multipl";
+        System.out.println(multipl);
+
+    }
+
+}
