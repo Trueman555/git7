4328796d9ddefd7784fedc7580a897baff2f1e00
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-21 09:56:28 +0300

    Update Task2.java

--- src/Task/Task2.java
+++ src/Task/Task2.java
@@ -6,7 +6,9 @@ public class Task2 {
 
 
     public static void main(String[] args){
-
+//Same question: why is it commented? 
+//+
+        
         Scanner scanner = new Scanner(System.in);
  //        System.out.println("Enter number1:"+ " ");
  //       int number1 = scanner.nextInt();
@@ -26,7 +28,7 @@ public class Task2 {
         int num1 = scanner.nextInt();
         System.out.println("Enter number2:"+ " ");
         int num2 = scanner.nextInt();
-
+//if - else is better. 
         String multipl=num1%num2==0? "Multipl":"Not multipl";
         System.out.println(multipl);
 
