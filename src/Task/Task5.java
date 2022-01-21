be1251aa36d85fa16f1e03f9cebccb5718332229
Mona Lisa <truman5555>
2022-01-21 21:56:13 +0300

    commit 1

---
+++ src/Task/Task5.java
@@ -0,0 +1,38 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int month=scanner.nextInt();
+
+        switch (month) {
+
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+                case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+                default:
+                    System.out.println("Trere is no such month");
+        }
+    }
+}
