b70f9034eaa06eb14932d402db2b8c465317fcbc
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 17:05:54 +0300

    Update CicleTask3.java

--- src/Cycle/CicleTask3.java
+++ src/Cycle/CicleTask3.java
@@ -1,7 +1,7 @@
 package Cycle;
 
 import java.util.Scanner;
-
+//naming could be better
 public class CicleTask3 {
     public static void main(String[] args){
 
@@ -11,7 +11,7 @@ public class CicleTask3 {
         System.out.println("Enter x:");
         int x =scanner.nextInt();
         int mult=0;
-
+//i like that you have predicted negative number's multiplication!:)
         for (int i=0; i!=y; i++){
             mult=mult+x;
 
