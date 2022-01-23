8bd51d2fd0a3d24e7d0947e3ce6ba36cd76758ac
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 17:06:48 +0300

    Update CicleTasr4.java

--- src/Cycle/CicleTasr4.java
+++ src/Cycle/CicleTasr4.java
@@ -1,9 +1,10 @@
 package Cycle;
-
+//USE FORMATTING - Ctrl+Alt+L
+//+
 public class CicleTasr4 {
     public static void main(String[] args){
         double inch=2.54;
-
+//what is 20?
         for (int i=1; i<=20; i++) {
 
             System.out.println(i+"inch"+"="+inch*i+"cm");
