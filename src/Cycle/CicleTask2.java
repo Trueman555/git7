9b9f5327340e98ca8371ded27f955a90ae36f3ba
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 17:09:13 +0300

    Update CicleTask2.java

--- src/Cycle/CicleTask2.java
+++ src/Cycle/CicleTask2.java
@@ -1,17 +1,18 @@
 package Cycle;
-
+//+'
 public class CicleTask2 {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
+                //ok, but there is a solution without if, we need to change something in for (int j = 0; j <= i; j++) 
                 if ( j >= i ) {
                     System.out.print(" ");
                 } else System.out.print("*");
             }
             System.out.println();
         }
-
+// a --> i, b-->j
         for (int a = 0; a < 5; a++) {
             for (int b = 0; b < 5; b++) {
                 if ( b > a ) {
