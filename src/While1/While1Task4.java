29eafe83e3dcf09c77558265bedb4a08b0ae7102
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-28 17:56:36 +0300

    Update While1Task4.java

--- src/While1/While1Task4.java
+++ src/While1/While1Task4.java
@@ -1,7 +1,9 @@
 package While1;
-
+//CONDITION
+//+-
 public class While1Task4 {
     public static void main(String[] args) {
+        //bad naming
         int x = 1;
         int sum = 1;
         while (x <= 99) {
@@ -9,18 +11,21 @@ public class While1Task4 {
             x += 2;
             sum += x;
         }
+        //if it is second solution, then leave a comment
         int y = 0;
+        //bad naming
         int sumy = 0;
         while (y <= 99) {
             y++;
+            //if(..){...}
             if ( y % 2 == 0 )
                 continue;
             sumy += y;
             System.out.println(sumy);
 
         }
-
-
+//no empty lines
+//no empty lines
     }
 
 }
