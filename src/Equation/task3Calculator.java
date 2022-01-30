87593aa7423c2f86f8a05adc893d773a05bb0391
Mona Lisa <truman5555>
2022-01-30 21:34:55 +0300

    commit 1

--- src/Equation/task3Calculator.java
+++ src/Equation/task3Calculator.java
@@ -5,38 +5,54 @@ import java.util.Scanner;
 public class task3Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter num1:");
-        double num1 = scanner.nextDouble();
-        System.out.println("Enter num2:");
-        double num2 = scanner.nextDouble();
+        boolean agreement;
+        agreement = true;
 
-        System.out.println("Enter operation:");
-        System.out.print("\n Enter an operator (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+        do {
+            System.out.println("Enter num1:");
+            double num1 = scanner.nextDouble();
+            System.out.println("Enter num2:");
+            double num2 = scanner.nextDouble();
+            System.out.print("Enter an operator (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+            double answer = 0;
 
-        double answer = 0;
+            switch (operator) {
+                case '+':
+                    answer = num1 + num2;
+                    break;
+                case '-':
+                    answer = num1 - num2;
+                    break;
+                case '*':
+                    answer = num1 * num2;
+                    break;
+                case '/':
+                    if ( num2 == 0 ) {
+                        System.out.println("Error");
+                    } else {
+                        answer = num1 / num2;
+                    }
+                    break;
+                default:
+            }
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);
+            System.out.println("Do you want to continue?");
+            String prover = scanner.next();
 
-        switch (operator) {
-            case '+':
-                answer = num1 + num2;
-                break;
-            case '-':
-                answer = num1 - num2;
-                break;
-            case '*':
-                answer = num1 * num2;
-                break;
-            case '/':
-                //division by 0?
-                answer = num1 / num2;
-                break;
-        }
-        //you do not have a loop! Your's program will just finish, but I need it to work till I enter stop word
-        System.out.print(num1 + " " + operator + " " + num2 + " = " + answer + "\n" + "Do you want to continue?");
-
+            if (prover.equals("yes") ) {
+                agreement = true;
+            }
+            if (prover.equals("no") ) {
+                agreement = false;
+            }
+        } while (agreement);
 
     }
 
 
 }
+
+
+
 
